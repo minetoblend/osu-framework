82868345dc9a67f6c9d8eb678a57c2f5ea0a7633
marvin <minetoblend@gmail.com>
2025-06-07 13:35:16 +0200

    Add CornerRadius to CircularProgress

--- osu.Framework/Resources/Shaders/sh_CircularProgress.fs
+++ osu.Framework/Resources/Shaders/sh_CircularProgress.fs
@@ -13,10 +13,10 @@ layout(location = 2) in highp vec2 v_TexCoord;
 
 layout(std140, set = 0, binding = 0) uniform m_CircularProgressParameters
 {
-    mediump float innerRadius;
+    mediump float thickness;
+    mediump float cornerRadius;
     mediump float progress;
     highp float texelSize;
-    bool roundedCaps;
 };
 
 layout(set = 1, binding = 0) uniform lowp texture2D m_Texture;
@@ -32,7 +32,7 @@ void main(void)
     highp vec2 wrappedCoord = wrap(v_TexCoord, v_TexRect);
     lowp vec4 textureColour = getRoundedColor(wrappedSampler(wrappedCoord, v_TexRect, m_Texture, m_Sampler, -0.9), wrappedCoord);
 
-    o_Colour = vec4(textureColour.rgb, textureColour.a * progressAlphaAt(pixelPos, progress, innerRadius, roundedCaps, texelSize));
+    o_Colour = vec4(textureColour.rgb, textureColour.a * progressAlphaAt(pixelPos, progress, thickness, cornerRadius, texelSize));
 }
 
 #endif
